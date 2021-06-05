301c39c7680798ccf6389b3cc3ab586cdf580f7c
serggarant <30666277+serggarant@users.noreply.github.com>
2021-06-05 14:40:30 +0300

    Create site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,32 @@
+node default {
+}
+
+node 'slave1.puppet' {
+   class { 'apache': }
+   
+   file { '/root/README':
+      ensure => absent,
+      }
+   
+   file { '/var/www/html/index.html':
+      ensure => file,
+      source => 'https://raw.githubusercontent.com/kozharskiy-vitaliy/puppet-hometask/production/files/index.html',
+      replace => false,
+      }
+}
+
+node 'slave2.puppet' {
+   class { 'apache::mod::php': }
+   
+   class { 'php': }
+   
+   file { '/root/README':
+      ensure => absent,
+      }
+   
+   file { '/var/www/html/index.php':
+      ensure => file,
+      source => 'https://raw.githubusercontent.com/kozharskiy-vitaliy/puppet-hometask/production/files/index.php',
+      replace => false,
+      }
+}
