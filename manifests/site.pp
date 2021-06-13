4c596749b27a4bde03db233f046c00086b3ef699
serggarant <30666277+serggarant@users.noreply.github.com>
2021-06-13 19:47:21 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,50 +1,29 @@
 node 'slave1.puppet' {
-   package { 'php':
-    ensure => installed, 
-            }
-  package { 'httpd':
-    ensure => installed, 
-            }
-
-  file { '/var/www/html/index.php':
-         ensure => file,
-         source => 'https://raw.githubusercontent.com/serggarant/puppet_conf/production/files/index.php',
-         replace => false,
-         notify => Service['httpd']
-          }
-  file {'/root/README':
+   class { 'apache': }
+   
+   file { '/root/README':
       ensure => absent,
-       }
-      service {'httpd':
-        ensure=> running,
-        
-      }  
-                    }
-  
-node 'slave2.puppet'{
-    class {'apache':
-    default_vhost => false, 
-          }
-      apache::vhost{'serg':
-            port    => '81',
-            docroot    => '/var/www/html',
-             }
-    file { '/var/www/html/index.html':
-         ensure => file,
-         source => 'https://raw.githubusercontent.com/serggarant/puppet_conf/production/files/index.html',
-         replace => false,
-         notify => Service['httpd']
-        file {'/root/README':
-         ensure => absent,
-         }
-             }
-node 'master.puppet' {
-class { "nginx": }
-   nginx::resource::server { 'serg':
-    listen_port => 81,
-    proxy       => 'slave2.puppet:81',
+      }
+   
+   file { '/var/www/html/index.html':
+      ensure => file,
+      source => 'https://raw.githubusercontent.com/serggarant/puppet_conf/production/files/index.html',
+      replace => false,
+      }
 }
- }
 
-      
-  
+node 'slave2.puppet' {
+   class { 'apache::mod::php': }
+   
+   class { 'php': }
+   
+   file { '/root/README':
+      ensure => absent,
+      }
+   
+   file { '/var/www/html/index.php':
+      ensure => file,
+      source => 'https://raw.githubusercontent.com/serggarant/puppet_conf/production/files/index.php',
+      replace => false,
+      }
+}
