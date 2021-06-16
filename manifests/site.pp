3d258d4abed072802dcbc7ae59da9ac8c70912d8
serggarant <30666277+serggarant@users.noreply.github.com>
2021-06-16 19:12:30 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,34 +1,32 @@
+node 'default' {} 
+
 node 'slave1.puppet' {
    class { 'apache':}
-   
-   file { '/root/README':
-      ensure => absent,
-      }
-   
-   file { '/var/www/html/index.html':
+    file { '/var/www/html/index.html':
       ensure => file,
       source => 'https://raw.githubusercontent.com/serggarant/puppet_conf/production/files/index.html',
       replace => false,
       }
-}
-
-class { 'apache::mod::php':}
-   apache::vhost{'web':
+    file {'/root/README':
+      ensure => absent,
+         }
+                   }
+node 'slave2.puppet'{
+   class { 'apache::mod::php':}
+   apache::vhost{'serg':
             port    => '81',
             docroot    => '/var/www/html',
              }
-   package { 'php':
+  package { 'php':
     ensure => installed, 
-            } 
-                 
-   
-   file { '/var/www/html/index.php':
-      ensure => file,
-      source => 'https://raw.githubusercontent.com/serggarant/puppet_conf/production/files/index.php',
-      replace => false,
-      }
-      
-   file {'/root/README':
+            }    
+                  
+  file { '/var/www/html/index.php':
+         ensure => file,
+         source => 'https://raw.githubusercontent.com/serggarant/puppet_conf/production/files/index.php',
+         replace => false,
+         }   
+    file {'/root/README':
       ensure => absent,
-         }       
+         }
 }
