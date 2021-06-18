e50ccffcba569ee1760d550a150f854f7f9cea19
serggarant <30666277+serggarant@users.noreply.github.com>
2021-06-18 22:00:16 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,38 +2,39 @@ node 'default' {}
 
 node 'slave1.puppet' {
    class { 'apache':}
-    file { '/var/www/html/index.html':
+      file { '/var/www/html/index.html':
       ensure => file,
       source => 'https://raw.githubusercontent.com/serggarant/puppet_conf/production/files/index.html',
       replace => false,
       }
-    file {'/root/README':
+      
+      file {'/root/README':
       ensure => absent,
-         }
+      }
 }
 node 'slave2.puppet'{
    class { 'apache::mod::php':}
    apache::vhost{'serg':
             port    => '81',
             docroot    => '/var/www/html',
-             }
-  package { 'php':
-    ensure => installed, 
-            }    
+   }
+   package { 'php':
+      ensure => installed, 
+   }    
                   
-  file { '/var/www/html/index.php':
+   file { '/var/www/html/index.php':
          ensure => file,
          source => 'https://raw.githubusercontent.com/serggarant/puppet_conf/production/files/index.php',
          replace => false,
-         }   
-    file {'/root/README':
+   }   
+   file {'/root/README':
       ensure => absent,
-         }
+   }
 }
 node 'master.puppet' {
-  include 'nginx'
+   include 'nginx'
       nginx::resource::server { '192.168.3.10':
       listen_port => 82,
       proxy       => '192.168.3.12:81',
-    }         
+      }         
 }
