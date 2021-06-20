5d2623b350e2b0d72b9c29c23f9dac1458dadc63
serggarant <30666277+serggarant@users.noreply.github.com>
2021-06-20 18:04:40 +0300

    Update mineserver.pp

--- site/profile/manifests/mineserver.pp
+++ site/profile/manifests/mineserver.pp
@@ -1,28 +1,40 @@
-class profile::mineserver {
-  
-file { '/opt/minecraft':
-  ensure => 'directory',
-     }
-file { '/opt/minecraft/server.jar':
-  ensure => file,
-  source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
-  replace => false,
-     }
-  
-file {'/opt/minecraft/eula.txt':
-  ensure => file,
-  content => 'eula=true'
-     }
-  
-file { '/etc/systemd/system/mineserver.service':
-  ensure => file,
-  source => 'puppet:///modules/mineserver/mineserver.service',
-  owner => 'root',
-  group => 'root',
-  mode  => '0644',
-     }
-  
-  service {'mineserver':
-  ensure => running,
-          }
+class profile::minecraft {
+
+file {'/eula.txt':
+      ensure => present,
+       content => "eula=true"         
+      }
+       
+class { 'java' :
+  package => 'java-1.8.0-openjdk-devel',
+      }
+      
+  package { 'jdk-16.0.1-2000:16.0.1-ga.x86_64':
+  provider => 'rpm',
+  ensure => present,
+  source => 'https://files01.tchspt.com/storage2/temp/jdk-16.0.1_linux-x64_bin.rpm',
+           }
+
+
+class { 'archive':
+  archives => { '/opt/minecraft/server.jar' => {
+                  'ensure' => 'present',
+                  'source'  => 'https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar',
+                                               }  
+               }
+       }
+ file {'/etc/systemd/system/minecraft.service':
+      ensure => present,
+      mode => "0664",
+      content => "[Unit]
+                  Description=Foo
+                  [Service]
+                  ExecStart= java -Xmx1024M -Xms1024M -jar /opt/minecraft/server.jar nogui
+                  [Install]
+                  WantedBy=multi-user.target",
+         }            
+   ~> service {'minecraft':
+     ensure    => 'running',
+     enable => 'true',
+              }
 }
